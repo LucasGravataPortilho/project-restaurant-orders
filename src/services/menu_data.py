a5a968c6ceaf4b9db3fdf949b2833a2652621112
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-19 09:31:09 -0300

    feat requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,28 @@
 # Req 3
+import csv
+
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        with open(source_path, encoding="utf-8", newline='') as file:
+            dataReader = csv.DictReader(file)
+
+            for line in dataReader:
+                dish_name = line['dish']
+                dish_price = float(line['price'])
+                dish_ingredient = line['ingredient']
+                dish_recipe_amount = int(line['recipe_amount'])
+
+                self.dishes.add(Dish(dish_name, dish_price))
+
+                next_dish = next(iter(self.dishes))
+
+                next_dish.add_ingredient_dependency(
+                    Ingredient(dish_ingredient),
+                    dish_recipe_amount,
+                    )
