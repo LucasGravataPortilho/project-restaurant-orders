a2da6335d51daec5d0380121d103508dca23458e
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-19 08:09:22 -0300

    feat. requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
-#iniciando projeto
+
 # Req 1
 def test_ingredient():
-    pass
+    correct_ingredient = Ingredient('queijo prato')
+    wrong_ingredient = Ingredient('presunto')
+    invalid_ingredient = Ingredient('')
+
+    # assert que instancias de ingredientes existem
+    assert correct_ingredient == correct_ingredient
+    assert correct_ingredient != wrong_ingredient
+    assert invalid_ingredient.name == ''
+
+    # assert se ingredientes tem alguma restrição
+    assert correct_ingredient.restrictions
+
+    # assert representação dos Ingredients
+    assert correct_ingredient.__repr__ == "Ingredient('queijo prato')"
